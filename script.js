1df62f8358a44f83d7078cccd517df3ca2f24690
Shyrudegal <shyrudegal@gmail.com>
2026-09-25 16:32:09 +0100

    mobile

--- script.js
+++ script.js
@@ -16,9 +16,9 @@ const toggle = document.getElementById("darkModeToggle");
 toggle.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
   if (document.body.classList.contains("dark-mode")) {
-    toggle.textContent = "Light☀️";
+    toggle.textContent = "Light ☀️";
   } else {
-    toggle.textContent = "Dark🌙";
+    toggle.textContent = "Dark 🌙";
   }
 
 const hour = new Date().getHours();
