df2839a3cb60964f836af3363c148a681d193948
Shyrudegal <shyrudegal@gmail.com>
2026-09-25 16:27:33 +0100

    sript file updatd

--- script.js
+++ script.js
@@ -16,8 +16,16 @@ const toggle = document.getElementById("darkModeToggle");
 toggle.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
   if (document.body.classList.contains("dark-mode")) {
-    toggle.textContent = "Light";
+    toggle.textContent = "Light☀️";
   } else {
-    toggle.textContent = "Dark";
+    toggle.textContent = "Dark🌙";
   }
+
+const hour = new Date().getHours();
+let greet = "Thanks for visiting!";
+if(hour < 12) greet = "Good morning! Thanks for visiting!";
+else if(hour < 18) greet = "Good afternoon! Thanks for visiting!";
+else greet = "Good evening! Thanks for visiting!";
+
+console.log(greet);
 });
